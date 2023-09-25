c2451231c6651a045f7333f62fba1c46913a65f9
Shahzaib Niaz <59477887+Lunister@users.noreply.github.com>
2023-09-25 19:30:59 +0500

    wip

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,21 @@ export class NewsItem extends Component {
 
     return (
       <div className="card" style={{ width: "18rem" }}>
-        <img src={!imageUrl?"/download.png": imageUrl} className="card-img-top" alt="..." />
+        <img
+          src={!imageUrl ? "/download.png" : imageUrl}
+          className="card-img-top"
+          alt="..."
+        />
         <div className="card-body">
-          <h5 className="card-title">{title?title:""}</h5>
-          <p className="card-text">{description?description:""}</p>
-          <a href={newsId} target="_blank" className="btn btn-primary btn-sm">
+          <h5 className="card-title">{title ? title : ""}</h5>
+          <p className="card-text">{description ? description : ""}</p>
+          <a
+            href={newsId}
+            target="_blank"
+            rel="noopener noreferrer" // Added for security when using target="_blank"
+            className="btn btn-primary btn-sm"
+            aria-label="Read more about this news"
+          >
             See more
           </a>
         </div>
